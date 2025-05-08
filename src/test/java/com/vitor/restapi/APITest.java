042e5673ffc388787233056eee18a6e5a0c96ffa
paladini-qa <paladini.qa@gmail.com>
2025-05-08 11:22:03 -0300

    feat: adicionando getPostOne

--- src/test/java/com/vitor/restapi/APITest.java
+++ src/test/java/com/vitor/restapi/APITest.java
@@ -4,14 +4,15 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ApiTest {
 
     private static final String BASE_URI = "https://jsonplaceholder.typicode.com";
-    private static final String POSTS_ENDPOINT = "/posts/1";
+    private static final String POSTS_ENDPOINT = "/posts";
 
     @Test
-    public void testGetPost() {
+    public void getPosts() {
         RestAssured.baseURI = BASE_URI;
 
         Response response = RestAssured
@@ -20,15 +21,26 @@ public class ApiTest {
 
         assertEquals(200, response.getStatusCode());
 
+        for (int i = 1; i <= 100; i++) {
+            assertEquals(String.valueOf(i), response.jsonPath().getString("[" + (i - 1) + "].id"));
+        }
+        assertNotNull(response.jsonPath().getString("userId"));
+        assertNotNull(response.jsonPath().getString("title"));
+        assertNotNull(response.jsonPath().getString("body"));
+    }
+    @Test
+    public void getPostOne(){
+        RestAssured.baseURI = BASE_URI;
+
+        Response response = RestAssured
+                .given()
+                .get(POSTS_ENDPOINT + "/1");
+
+        assertEquals(200, response.getStatusCode());
+
         assertEquals("1", response.jsonPath().getString("id"));
         assertEquals("1", response.jsonPath().getString("userId"));
-        assertEquals(
-                "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-                response.jsonPath().getString("title")
-        );
-        assertEquals(
-                "quia et suscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit",
-                response.jsonPath().getString("body")
-        );
+        assertEquals("sunt aut facere repellat provident occaecati excepturi optio reprehenderit", response.jsonPath().getString("title"));
+        assertEquals("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto", response.jsonPath().getString("body"));
     }
 }
