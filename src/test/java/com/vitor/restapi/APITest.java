c8c4ff49e44c348d99cbaa192a986444d646ee15
paladini-qa <paladini.qa@gmail.com>
2025-05-08 11:03:40 -0300

    feat/validar-response-body

--- src/test/java/com/vitor/restapi/APITest.java
+++ src/test/java/com/vitor/restapi/APITest.java
@@ -5,14 +5,30 @@ import io.restassured.response.Response;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class APITest{
+public class ApiTest {
+
+    private static final String BASE_URI = "https://jsonplaceholder.typicode.com";
+    private static final String POSTS_ENDPOINT = "/posts/1";
 
     @Test
-    public void testGetPost(){
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    public void testGetPost() {
+        RestAssured.baseURI = BASE_URI;
 
-        Response response = RestAssured.given().get("/posts/1");
-        
+        Response response = RestAssured
+                .given()
+                .get(POSTS_ENDPOINT);
+
         assertEquals(200, response.getStatusCode());
+
+        assertEquals("1", response.jsonPath().getString("id"));
+        assertEquals("1", response.jsonPath().getString("userId"));
+        assertEquals(
+                "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                response.jsonPath().getString("title")
+        );
+        assertEquals(
+                "quia et suscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit\nsuscipit",
+                response.jsonPath().getString("body")
+        );
     }
 }
